92626937c3d4cb3cd5e6370fa069c225bd30fdc2
Yann Klein <yann.klein@me.com>
2019-10-08 11:23:35 +0900

    improve UI

--- README.md
+++ README.md
@@ -3,16 +3,10 @@ This example will show how we can teach an AI to play a simple game using the Q-
 
 This is implemented in pure Ruby without any external dependencies.
 
-To run the game and play a human run:
+To run the game Human vs AI:
 
 ```
 $ ruby run.rb
-```
-
-To run the game and let the AI play run:
-
-```
-$ ruby run-q.rb
 ```
 
 For more information about how this is implemented see [Teaching an AI to play a simple game using q-learning](http://www.practicalai.io/teaching-ai-play-simple-game-using-q-learning/).
--- run.rb
+++ run.rb
@@ -1,6 +1,52 @@
 require './game.rb'
+require './q_learning_player.rb'
 require './player.rb'
 
-p = Player.new
-g = Game.new( p )
-g.run
+human = Player.new
+robot = QLearningPlayer.new
+g = Game.new(human)
+
+# First human plays
+puts '   _                                           _            '
+puts '  / )/ _ _  _ _   _   _/  /   /_  _   _// _   /_| /     _   '
+puts ' (__/)(-(-_) (-  (//)(/  /)()((-_) ,  //)(-  (  |/  ((/(/(/ '
+puts '________________________________________________________ / '
+puts''
+puts 'You will play a simple game against an AI'
+print "[press a key to continue]\r"
+STDIN.getch
+puts "You are [P] the player, use the A,W,S,D keys to move through the grid"
+print "[press a key to continue]\r"
+STDIN.getch
+puts 'You need to reach [C] the cheese while avoiding the O holes'
+print "[press a key to continue]\r"
+STDIN.getch
+puts 'You need to score 5 points with as less move as possible to win the game'
+print "[press a key to continue]\r"
+STDIN.getch
+puts 'Reaching [C] gives you 1 point, falling in a O takes you 1 point!'
+puts 'Ready?'
+print "[press a key to PLAY the game]\r"
+STDIN.getch
+system "clear"
+g.player = human
+puts "\r#{g.run}"
+print 'Now the AI'
+sleep(0.5)
+print '.'
+sleep(0.5)
+print '.'
+sleep(0.5)
+puts '.'
+# Then AI plays
+g.player = robot
+g.winning_sentence = ''
+robot.game = g
+result = ''
+100.times do
+  result = g.run
+  g.reset
+end
+
+# p.print_table
+puts result
